a418a3cfae5c4c3dac391bd5576b4de473c9e145
antfarmar <markfarrugiamail@gmail.com>
2017-07-20 20:42:58 -0400

    Initial commit

---
+++ implementation/gradingstudents/gradingstudents.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var (
+	//fileIn        = os.Stdin
+	testData        = `C:\Users\maf\go\src\hackerrank\implementation\gradingstudents\input\input00.txt`
+	fileIn, openErr = os.Open(testData)
+	fileOut         = os.Stdout
+	rw              = bufio.NewReadWriter(bufio.NewReader(fileIn), bufio.NewWriter(fileOut))
+	bytesWritten    = 0
+)
+
+// MAIN ==================================================
+
+func main() {
+	check(openErr)
+	defer report()
+	defer fileOut.Close()
+	defer fileIn.Close()
+	defer rw.Flush()
+
+	N := atois(readfields())[0]
+	for i := 0; i < N; i++ {
+		grade := atois(readfields())[0]
+		write(fmt.Sprintf("%v\n", solution(grade)))
+	}
+}
+
+// SOLUTION ==============================================
+
+func solution(grade int) (newGrade int) {
+	newGrade = grade
+	if grade >= 38 {
+		if grade%5 > 2 {
+			newGrade += (5 - grade%5)
+		}
+	}
+	return
+}
+
+// IO ====================================================
+
+func write(s string) {
+	n, err := rw.WriteString(s)
+	check(err)
+	bytesWritten += n
+}
+
+func report() {
+	fmt.Fprintf(os.Stderr, "<wrote %v bytes>", bytesWritten)
+}
+
+func readline() (line string) {
+	line, err := rw.ReadString('\n')
+	check(err)
+	line = strings.TrimSpace(line)
+	return
+}
+
+func readfields() (fields []string) {
+	line, err := rw.ReadString('\n')
+	check(err)
+	fields = strings.Fields(line)
+	return
+}
+
+func atois(S []string) (I []int) {
+	I = make([]int, len(S))
+	for i, a := range S {
+		n, err := strconv.Atoi(a)
+		check(err)
+		I[i] = n
+	}
+	return
+}
+
+func check(err error) {
+	if err != nil {
+		if err.Error() == "EOF" {
+			fmt.Fprintln(os.Stderr, "<EOF reached>")
+		} else {
+			log.Fatal(err)
+		}
+	}
+}
