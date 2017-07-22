7925b1d7b32ecd6b6a8ac3aedbfafdb6367c56f6
antfarmar <markfarrugiamail@gmail.com>
2017-07-21 23:19:28 -0400

    initial commit

---
+++ strings/camelcase/camelcase.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var (
+	//fileIn        = os.Stdin
+	testData        = `C:\Users\maf\go\src\hackerrank\strings\camelcase\input\input00.txt`
+	fileIn, openErr = os.Open(testData)
+	fileOut         = os.Stdout
+	rw              = bufio.NewReadWriter(bufio.NewReader(fileIn), bufio.NewWriter(fileOut))
+	bytesWritten    = 0
+)
+
+// MAIN ==================================================
+
+func main() {
+	check(openErr)
+	defer report()
+	defer fileOut.Close()
+	defer fileIn.Close()
+	defer rw.Flush()
+
+	S := readline()
+	write(fmt.Sprintf("%v\n", solution(S)))
+}
+
+// SOLUTION ==============================================
+
+func solution(s string) (wordCnt int) {
+	wordCnt++
+	for _, c := range s {
+		if c >= 65 && c <= 90 { //ASCII: A=65, Z=90
+			wordCnt++
+		}
+	}
+	return
+}
+
+// IO ====================================================
+
+func write(s string) {
+	n, err := rw.WriteString(s)
+	check(err)
+	bytesWritten += n
+}
+
+func report() {
+	fmt.Fprintf(os.Stderr, "<wrote %v bytes>", bytesWritten)
+}
+
+func readline() (line string) {
+	line, err := rw.ReadString('\n')
+	check(err)
+	line = strings.TrimSpace(line)
+	return
+}
+
+func readfields() (fields []string) {
+	line, err := rw.ReadString('\n')
+	check(err)
+	fields = strings.Fields(line)
+	return
+}
+
+func atois(S []string) (I []int) {
+	I = make([]int, len(S))
+	for i, a := range S {
+		n, err := strconv.Atoi(a)
+		check(err)
+		I[i] = n
+	}
+	return
+}
+
+func check(err error) {
+	if err != nil {
+		if err.Error() == "EOF" {
+			fmt.Fprintln(os.Stderr, "<EOF reached>")
+		} else {
+			log.Fatal(err)
+		}
+	}
+}
